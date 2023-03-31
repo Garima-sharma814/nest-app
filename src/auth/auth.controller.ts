ef51ff34ac2dac86564a384de41a430c49b78186
Garima Sharma <sharmagarima814@gmail.com>
2023-03-31 09:19:16 +0530

    Add: Http status codes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDTO } from './dto';
 
@@ -6,12 +6,14 @@ import { AuthDTO } from './dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('signup')
-  // using dtos (data transfer objects) for validations
+  //* using dtos (data transfer objects) for validations
   signup(@Body() dto: AuthDTO) {
     return this.authService.signUp(dto);
   }
 
+  @HttpCode(HttpStatus.OK) //* post request by default send 201 and get request send 200
   @Post('signin')
   signin(@Body() dto: AuthDTO) {
     return this.authService.signIn(dto);
