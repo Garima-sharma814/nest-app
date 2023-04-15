981c7c0f5abb12c0500c954e9036850ef7cc5642
Garima Sharma <sharmagarima814@gmail.com>
2023-04-15 20:23:17 +0530

    complete: user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { getUser } from 'src/auth/decorator';
 import { JwtGaurd } from 'src/auth/gaurd';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGaurd) // gaurd is kind of a middleware
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('me')
   getMe(@getUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@getUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
